f9f361aee97a33d6124a98f09811f3e3d734f9d5
Alicja <mazur.alicja4@gmail.com>
2020-12-27 23:56:20 +0100

    edit files

--- artifacts/redshift-based-etl/SQL/load_redshift.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO ratings
-SELECT * FROM ratings.ratings;
-
-
-INSERT INTO tmp_table
-SELECT AVG(rating) as avg_rating, year, title, company FROM ratings.ratings GROUP BY company, title, year;
